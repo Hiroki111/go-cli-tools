fd1cdf55094d54d437ac58697fb3ae5e44b39e94
Hiroki111 <9676341+Hiroki111@users.noreply.github.com>
2025-10-21 08:32:06 +0200

    word-counter

---
+++ word-counter/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	fmt.Println(count(os.Stdin))
+}
+
+func count(r io.Reader) int {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanWords)
+	wordCount := 0
+
+	for scanner.Scan() {
+		wordCount++
+	}
+
+	return wordCount
+}
