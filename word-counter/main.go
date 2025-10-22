0b5606fee00f4a923a8dfaf66e9e06d77604beb5
Hiroki111 <9676341+Hiroki111@users.noreply.github.com>
2025-10-22 07:04:38 +0200

    added l flag to the counter

--- word-counter/main.go
+++ word-counter/main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	fmt.Println(count(os.Stdin))
+	// "l" means that it counts lines. The default value is false, so the default behaviour is to count words.
+	lines := flag.Bool("l", false, "Count lines")
+	flag.Parse()
+	fmt.Println(count(os.Stdin, *lines))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
 	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanWords)
+
+	// If the count lines flag is not set, it counts words so it defines
+	// the scanner split type to words (the default behaviour is to split by lines)
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
+
 	wordCount := 0
 
 	for scanner.Scan() {
