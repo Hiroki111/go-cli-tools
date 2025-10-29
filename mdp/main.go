f39907c66c859e44947dd64142369beb5b283e1d
Hiroki111 <9676341+Hiroki111@users.noreply.github.com>
2025-10-29 07:43:56 +0100

    created main.go and its tests

--- mdp/main.go
+++ mdp/main.go
@@ -1,1 +1,71 @@
 package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday/v2"
+)
+
+const (
+	header = `<!DOCTYPE html>
+	<html>
+		<head>
+			<meta http-equiv="content-type" content="text/html; charset=utf-8">
+			<title>Markdown Preview Tool</title>
+		</head>
+		<body>`
+	footer = `
+	   </body>
+	</html>
+	`
+)
+
+func main() {
+	fileName := flag.String("file", "", "Markdown file to preview")
+	flag.Parse()
+
+	if *fileName == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if err := run(*fileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(fileName string) error {
+	input, err := os.ReadFile(fileName)
+	if err != nil {
+		return err
+	}
+
+	htmlData := parseContent(input)
+
+	outName := fmt.Sprintf("%s.html", filepath.Base(fileName))
+	fmt.Println(outName)
+
+	return saveHTML(outName, htmlData)
+}
+
+func parseContent(input []byte) []byte {
+	output := blackfriday.Run(input)
+	body := bluemonday.UGCPolicy().SanitizeBytes(output)
+
+	var buffer bytes.Buffer
+	buffer.WriteString(header)
+	buffer.Write(body)
+	buffer.WriteString(footer)
+
+	return buffer.Bytes()
+}
+
+func saveHTML(outputFileName string, data []byte) error {
+	return os.WriteFile(outputFileName, data, 0644)
+}
