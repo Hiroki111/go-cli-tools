beb0984d0d2b49684de58b9bfb10e2270e84789d
Hiroki111 <9676341+Hiroki111@users.noreply.github.com>
2025-11-15 11:09:48 +0100

    refactor the run function for CPU-bound operations

--- col-stats/main.go
+++ col-stats/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"runtime"
 	"sync"
 )
 
@@ -42,38 +43,52 @@ func run(fileNames []string, op string, column int, out io.Writer) error {
 
 	consolidate := make([]float64, 0)
 
+	// NOTE: See this file in the previous commit. Perhaps using filesCh doesn't increase the performance.
+	// I did benchmarking with -benchmem, but ns/op became worse unlike what I saw in the book.
+	// The book's author says this filesCh approach is used because this program's goroutines are CPU-bound.
+	// However, this program doesn't do heavy computation (e.g., Parsing several GB of files, doing expensive math).
+	// To me, the goroutines here seem IO-bound, rather than CPU-bound.
+	filesCh := make(chan string)
 	resultCh := make(chan []float64)
 	errCh := make(chan error)
 	doneCh := make(chan struct{})
 
 	wg := sync.WaitGroup{}
 
-	for _, fileName := range fileNames {
+	go func() {
+		defer close(filesCh)
+		for _, fileName := range fileNames {
+			filesCh <- fileName
+		}
+	}()
+
+	for i := 0; i < runtime.NumCPU(); i++ {
 		wg.Add(1)
 
-		go func(fileName string) {
+		go func() {
 			defer wg.Done()
+			for fileName := range filesCh {
+				// os.Open -> async
+				file, err := os.Open(fileName)
+				if err != nil {
+					errCh <- fmt.Errorf("Cannot open file: %w", err)
+					return
+				}
 
-			// os.Open -> async
-			file, err := os.Open(fileName)
-			if err != nil {
-				errCh <- fmt.Errorf("Cannot open file: %w", err)
-				return
+				// csv2float -> async (It reads a file)
+				data, err := csv2float(file, column)
+				if err != nil {
+					errCh <- err
+				}
+
+				// file.Close -> async
+				if err := file.Close(); err != nil {
+					errCh <- err
+				}
+
+				resultCh <- data
 			}
-
-			// csv2float -> async (It reads a file)
-			data, err := csv2float(file, column)
-			if err != nil {
-				errCh <- err
-			}
-
-			// file.Close -> async
-			if err := file.Close(); err != nil {
-				errCh <- err
-			}
-
-			resultCh <- data
-		}(fileName)
+		}()
 	}
 
 	go func() {
