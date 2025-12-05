46de5e207073494f324fa1d4d3052545fbb057b7
Hiroki111 <9676341+Hiroki111@users.noreply.github.com>
2025-12-05 08:15:54 +0100

    added notify definition

---
+++ notify/notify.go
@@ -0,0 +1,23 @@
+package notify
+
+const (
+	SeverityLow = iota
+	SeverityNormal
+	SeverityUrgent
+)
+
+type Severity int
+
+type Notify struct {
+	title    string
+	message  string
+	severity Severity
+}
+
+func New(title, message string, severity Severity) *Notify {
+	return &Notify{
+		title:    title,
+		message:  message,
+		severity: severity,
+	}
+}
